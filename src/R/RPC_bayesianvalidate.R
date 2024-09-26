ea310758032cc2f45f47d4ab27e1b34273f2579a
Pedro Mateus <pedro.mateus@maastro.nl>
2024-09-26 16:55:52 +0200

    Refactor performance evaluation to share the imputation model + use exact method to predict

--- src/R/RPC_bayesianvalidate.R
+++ src/R/RPC_bayesianvalidate.R
@@ -2,7 +2,8 @@ compute_metrics <- function (df, model, pred_col) {
     # Compute the ROC AUC, PR AUC and confusion matrix
     result <- list(n_obs=nrow(df))
 
-    preds <- predict(model, node=pred_col, data=df, method="bayes-lw", prob=TRUE)
+    # preds <- predict(model, node=pred_col, data=df, method="bayes-lw", prob=TRUE)
+    preds <- predict(model, node=pred_col, data=df, method="exact", prob=TRUE)
 
     # ROC evaluation in case of a binary predictor
     if (length(levels(df[[pred_col]])) == 2 && all(table(df[[pred_col]]) > 0)) {
@@ -31,10 +32,16 @@ compute_metrics <- function (df, model, pred_col) {
     return(result)
 }
 
-validate_data <- function(df, model, pred_col, factors_by_column, config, train_set=FALSE, validating=FALSE, external_set=FALSE) {
-    df <- factor_dataframe(df, config, train_set, external_set, factors_by_column, validating)
+validate_data <- function(df, model, pred_col, factors_by_column, config, train_set=FALSE, validating=FALSE, external_set=FALSE, imputation_model=NULL) {
+    bn_impute <- ("bn_impute" %in% names(config) && config[["bn_impute"]])
+    out <- factor_dataframe(df, config, train_set, external_set, factors_by_column, validating, bn_impute, imputation_model)
+    df <- out$data
+    if (bn_impute) {
+        vtg::log$info("Impute using bnlearn")
+        df <- bnlearn::impute(model, data = df, method="exact")
+    }
     vtg::log$info("Got {nrow(df)} rows in this node's data")
-    return(compute_metrics(df, model, pred_col))
+    return(list("metrics"=compute_metrics(df, model, pred_col), "imputation_model"=out$imputation_model))
 }
 
 RPC_bayesianvalidate <- function(df, model, pred_col, factors_by_column, config, external_set=FALSE) {
@@ -45,34 +52,38 @@ RPC_bayesianvalidate <- function(df, model, pred_col, factors_by_column, config,
         if (external_set) {
             vtg::log$info("Testing set")
             config[["impute"]] <- FALSE
-            performance_result[["test_set"]] <- validate_data(
+            performance_result[["test_set"]] <- (validate_data(
                 df, model, pred_col, factors_by_column, config, external_set=TRUE, validating=TRUE
-            )
+            ))$metrics
         } else {
             # Training set
             if ("impute" %in% names(config) && config[["impute"]]) {
                 vtg::log$info("Imputed data")
                 set_seed_config(config)
-                performance_result[["train_set_imputed"]] <- validate_data(
+                out <- validate_data(
                     df, model, pred_col, factors_by_column, config, train_set=TRUE
                 )
+                performance_result[["train_set_imputed"]] <- out$metrics
+                imputation_model <- out$imputation_model
                 set_seed_config(config)
-                performance_result[["validation_set_imputed"]] <- validate_data(
-                    df, model, pred_col, factors_by_column, config, train_set=FALSE
-                )
+                performance_result[["validation_set_imputed"]] <- (validate_data(
+                    df, model, pred_col, factors_by_column, config, train_set=FALSE, imputation_model=imputation_model
+                ))$metrics
             }
             vtg::log$info("Original dataset")
             config[["impute"]] <- FALSE
             # The validating flag will only keep the rows with less than 'nan_limit'
             # number of missing values (by default 1)
             set_seed_config(config)
-            performance_result[["train_set"]] <- validate_data(
+            out <- validate_data(
                 df, model, pred_col, factors_by_column, config, train_set=TRUE, validating=TRUE
             )
+            performance_result[["train_set"]]  <- out$metrics
+            imputation_model <- out$imputation_model
             set_seed_config(config)
             performance_result[["validation_set"]] <- validate_data(
-                df, model, pred_col, factors_by_column, config, train_set=FALSE, validating=TRUE
-            )
+                df, model, pred_col, factors_by_column, config, train_set=FALSE, validating=TRUE, imputation_model=imputation_model
+            )$metrics
         }
         return(performance_result)
     }, error = function(e) {
