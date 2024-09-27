99271f4127258c97021d09e51ae2a30f31863d12
Pedro Mateus <pedro.mateus@maastro.nl>
2024-09-27 11:20:45 +0200

    Refactor to create the folds + summary statistics

--- src/R/utils.R
+++ src/R/utils.R
@@ -1,14 +1,115 @@
-get_data_split <- function(config, size) {
-    split <- 1
-    if ("data_split" %in% names(config)) {
-        split <- config[["data_split"]]
+summary_stats <- function(df) {
+    metrics = list(
+        "n" = nrow(df)
+    )
+    df <- data.frame(lapply(df , as.factor))
+    for (variable in colnames(df)) {
+        if (is.factor(df[[variable]]) && length(levels(as.factor(df[[variable]]))) < 5) {
+            distribution = table(df[[variable]])
+            distribution[distribution < 5] = 5
+            metrics[[variable]] <- list(
+                "null_values" = sum(is.na(df[[variable]])),
+                "stats" = distribution
+            )
+        }
     }
-    dt = sort(sample(size, size * split))
+    return(metrics)
+}
+
+get_indexes <- function(size, folds, k, sampler=c()) {
+    interval = round(size/folds)
+    start = if (k == 1) 1 else 1 + interval * (k - 1)
+    end = if (k == folds) size else interval * k
+    if (length(sampler) > 0) {
+        indexes = sample(sampler, size)
+    } else {
+        indexes = sample(size, size)
+    }
+    dt = indexes[!indexes %in% indexes[start:end]]
+    print(dt)
     return(dt)
 }
 
-factor_dataframe <- function(df, config, train=TRUE, external_set=FALSE, factors_by_column=list(), validating=FALSE) {
+get_data_split <- function(config, size, pos_ind, neg_ind) {
+    dt <- c()
+    print(config[["k_fold_p"]])
+    if ("k_fold_p" %in% names(config) && config[["k_fold_p"]] == TRUE ) {
+        print("K fold - partitioned")
+        folds <- config[["k_fold"]]
+        k <- config[["k"]]
+        print(k)
+        print(pos_ind)
+        print(neg_ind)
+        dt <- c(
+            get_indexes(length(pos_ind), folds, k, pos_ind),
+            get_indexes(length(neg_ind), folds, k, neg_ind)
+        )
+    } else if ("k_fold" %in% names(config) && "k" %in% names(config)) {
+        print("K fold")
+        folds <- config[["k_fold"]]
+        k <- config[["k"]]
+        dt <- get_indexes(size, folds, k)
+        #interval = round(size/folds)
+        #start = if (k == 1) 1 else 1 + interval * (k - 1)
+        #end = if (k == folds) size else interval * k
+        #indexes = sample(size, size)
+        #dt = indexes[!indexes %in% indexes[start:end]]
+    } else {
+        split <- 1
+        if ("data_split" %in% names(config)) {
+            split <- config[["data_split"]]
+        }
+        dt = sort(sample(size, size * split))
+    }
+    return(dt)
+}
+
+factor_dataframe <- function(df, config, train=TRUE, external_set=FALSE, factors_by_column=list(), validating=FALSE, bn_impute=FALSE, imputation_model=NULL) {
+    age_tertiles_rome <- c(20.00,	60.3,	71.0,	100)
+    age_quartiles_rome <- c(20.00,	57.0,	66.0,	72.0,	100)
+    age_tertiles_maastro <- c(20.00, 65.00,	75.00,	100)
+    age_quartiles_maastro <- c(20.00,	62.00,	70.00,	77.00,	100)
+    age_tertiles_global <- (age_tertiles_maastro * 432 + age_tertiles_rome * 470) / (432 + 470)
+    age_quartiles_global <- (age_quartiles_maastro * 432 + age_quartiles_rome * 470) / (432 + 470)
+    df$age_discretised <- cut(as.numeric(df$Age), age_tertiles_global, include.lowest = TRUE)
+    df$age_discretised_q <- cut(as.numeric(df$Age), age_quartiles_global, include.lowest = TRUE)
+
+    gtv_tertiles_rome <- c(2000,	21246.634,	47868.789,	800000)
+    gtv_quartiles_rome <- c(2000,	17971.273,	32054.622,	52764.269,	800000)
+    gtv_tertiles_maastro <- c(2000,	39085.259,	66849.359,	800000)
+    gtv_quartiles_maastro <- c(2000,	31991.594,	50794.598,	79404.664,	800000)
+
+    gtv_tertiles_global <- (gtv_tertiles_maastro * 426 + gtv_tertiles_rome * 86) / (426 + 86)
+    gtv_quartiles_global <- (gtv_quartiles_maastro * 426 + gtv_quartiles_rome * 86) / (426 + 86)
+    df$gtv_discretised <- cut(as.numeric(df$GTV), gtv_tertiles_global, c("small", "medium", "large"), include.lowest = TRUE)
+    df$gtv_discretised_q <- cut(as.numeric(df$GTV), gtv_quartiles_global, c("micro", "small", "medium", "large"), include.lowest = TRUE)
+
+    gtv_tertiles <- quantile(as.numeric(df$GTV), probs = seq(from = 0, to = 1, by = 0.33), na.rm = TRUE)
+    gtv_quartiles <- quantile(as.numeric(df$GTV), probs = seq(from = 0, to = 1, by = 0.25), na.rm = TRUE)
+    gtv_tertiles <- c(3000, gtv_tertiles[[2]], gtv_tertiles[[3]], 800000)
+    gtv_quartiles <- c(3000, gtv_quartiles[[2]], gtv_quartiles[[3]], gtv_quartiles[[4]], 800000)
+    df$gtv_discretised_local_q <- cut(as.numeric(df$GTV), gtv_quartiles, c("micro", "small", "medium", "large"), include.lowest = TRUE)
+    df$gtv_discretised_local <- cut(as.numeric(df$GTV), gtv_tertiles, c("small", "medium", "large"), include.lowest = TRUE)
+
+    distance_tertiles_rome <- c(30.00,	60.0,	85.0,	230.00)
+    distance_quartiles_rome <- c(30.00,	50.0,	70.0,	90.0,	230.00)
+    distance_tertiles_maastro <- c(30.00,	55.4,	90.0,	230.00)
+    distance_quartiles_maastro <- c(30.00,	45.0,	70.0,	100.0,	230.00)
+    distance_tertiles_global <- (distance_tertiles_maastro * 375 + distance_tertiles_rome * 447) / (375 + 447)
+    distance_quartiles_global <- (distance_quartiles_maastro * 375 + distance_quartiles_rome * 447) / (375 + 447)
+    df$distance_discretised <- cut(as.numeric(df$distance), distance_tertiles_global, c("small", "medium", "large"), include.lowest = TRUE)
+    df$distance_discretised_q <- cut(as.numeric(df$distance), distance_quartiles_global, c("micro", "small", "medium", "large"), include.lowest = TRUE)
+
     pred_col <- config[["pred_col"]]
+    # Convert to factors
+    df <- data.frame(lapply(df , as.factor))
+    # Differences between the nodes' values
+    df[[pred_col]] <- droplevels(factor(
+        df[[pred_col]],
+        levels=c("no", "0", 0, "yes", "1", 1),
+        labels=c(0, 0, 0, 1, 1, 1)
+    ))
+    # Limit for the number of missing fields for each patient
     nan_limit <- 1
     if ("nan_limit" %in% names(config)) {
         nan_limit <- config[["nan_limit"]]
@@ -17,18 +118,33 @@ factor_dataframe <- function(df, config, train=TRUE, external_set=FALSE, factors
     if ("exclude" %in% names(config)) {
         df <- df[,!(names(df) %in% config[["exclude"]])]
     }
+    summary <- summary_stats(df)
+    # Only include rows with a number of missing fields within the limit
+    #if (validating) {
+    cnt_na <- apply(df, 1, function(z) sum(is.na(z)))
+    df <- df[cnt_na <= nan_limit, ]
+    #}
+
     # Split the data according to the options
     if (!external_set) {
         # Perform the split for the rows that have an Outcome
         # All columns without an outcome will be used for training
         df_pred <- df[!is.na(df[pred_col]),]
-        split_pred <- get_data_split(config, nrow(df_pred))
+        # TODO: remove the magic numbers
+        split_pred <- get_data_split(config, nrow(df_pred), which(df_pred[pred_col] == 1), which(df_pred[pred_col] == 0))
+        print(split_pred)
         if (train) {
             if ("impute" %in% names(config) && config[["impute"]]) {
-                df <- rbind(df_pred[split_pred,], df[is.na(df[pred_col]),])
+                df_no_pred <- df[is.na(df[pred_col]),]
+                fraction_without_pred <- 1
+                if ("impute_n" %in% names(config)) {
+                    fraction_without_pred <- config[["impute_n"]]
+                }
+                df <- rbind(df_pred[split_pred,], df_no_pred[sample(nrow(df_no_pred), round(nrow(df_no_pred) * fraction_without_pred)), ])
             } else {
                 df <- df_pred[split_pred,]
             }
+            #df <- df[sample(nrow(df)),]
         } else {
             df <- df_pred[-split_pred,]
         }
@@ -36,30 +152,56 @@ factor_dataframe <- function(df, config, train=TRUE, external_set=FALSE, factors
         # Only the rows with the outcome available
         df <- df[!is.na(df[pred_col]),]
     }
-    df <- data.frame(lapply(df , as.factor))
-    # Imputation
-    if (validating) {
-        cnt_na <- apply(df, 1, function(z) sum(is.na(z)))
-        df <- df[cnt_na < nan_limit, ]
-    }
-    if (("impute" %in% names(config) && config[["impute"]]) || (validating && nan_limit > 1)) {
+
+    if (nrow(df) > 0 && (("impute" %in% names(config) && config[["impute"]]) || (validating && nan_limit > 0 && !bn_impute))) {
+        print("Imputation performed.")
         m <- 5
         if ("impute_m" %in% names(config)) {
             m <- config[["impute_m"]]
         }
-        df <- mice::complete(mice::mice(df, m=m), "stacked")
-    } else {
+        if (is.null(imputation_model)) {
+            print("New imputation model")
+            imputation_model <- mice::mice(df, m=m, maxit = 30)
+            df <- mice::complete(imputation_model, action="stacked")
+        } else {
+            # Validation set can create the following error:
+            # Error in mice.impute.polyreg(y = structure(c(3L, 2L, 2L, 2L, 5L, 2L, 2L, : dims [product 4] do not match the length of object [5]\n
+            # Not enough data to represent ...
+            imputation_model.mids <- mice::mice.mids(imputation_model, newdata = df, maxit = 1)
+            df <- mice::complete(imputation_model.mids, action="stacked")
+            #df_full <- mice::complete(imputation_model.mids, action="stacked")
+            #df <- mice::mice.reuse(imputation_model, newdata = df, maxit = 0)
+            # df_indexes <- c()
+            # for (i in 1:m) {
+            #     start <- 1 + i * nrow(imputation_model$data) + (i - 1) * nrow(df)
+            #     end <- start + nrow(df) - 1
+            #     df_indexes <- c(df_indexes, c(start:end))
+            # }
+            # df <- df_full[df_indexes, ]
+        }
+        # imputation_model.mids <- mice::mice.mids(imputation_model, newdata = df, maxit = 0)
+    }
+
+    # Imputed afterwards with bnlearn or keep the missing values (for methods that
+    # can handle)
+    if (!bn_impute && (!("keep_nan" %in% names(config)) || !config[["keep_nan"]])) {
+        print("Ommiting the rows with missing fields")
         df <- na.omit(df)
     }
     # Factors can only be included after imputation
     if (length(factors_by_column) > 0) {
         for (column in names(factors_by_column)) {
-            if (column %in% names(df)) {
-                levels(df[[column]]) <- factor(factors_by_column[[column]])
+            if (column %in% names(df) && column != pred_col) {
+                # Guaranteeing the same order to avoid switching the categories
+                df[[column]] <- factor(df[[column]], levels = factors_by_column[[column]])
+                #levels(df[[column]]) <- factor(
+                #    unique(c(levels(df[[column]]), factors_by_column[[column]]))
+                #)
             }
         }
     }
-    return(df)
+    print("Number of rows with missing data: {sum(is.na(df))}")
+    return(list("data"=df, "imputation_model"=imputation_model, "summary"=summary))
 }
 
 parse_roc <- function(partial_rocs) {
